3ba53ef18fbae8a522a3d8516aa4a081212c30f5
Craig Morton <CraigMorton92@gmail.com>
2016-06-11 20:56:42 +0100

    adds min, max and step attributes to settings input text boxes

--- react_webapp/client/src/components/settings/SettingsContainer.jsx
+++ react_webapp/client/src/components/settings/SettingsContainer.jsx
@@ -9,13 +9,13 @@ const SettingsContainer = ({
     <div className="react-container">
     <h5>Settings</h5>
     <label for="numPlayers">Number of Players (3-8):</label>
-    <input type="number" id="numPlayers" disabled={settingsDisabled} onChange={listeners.onUpdateSetting} defaultValue={state.numPlayers}/>
+    <input type="number" id="numPlayers" disabled={settingsDisabled} onChange={listeners.onUpdateSetting} defaultValue={state.numPlayers} step="1" min="3" max="8"/>
     <label for="playerMaxHealth">Each Player's Max Health:</label>
-    <input type="number" id="playerMaxHealth" disabled={settingsDisabled} onChange={listeners.onUpdateSetting} defaultValue={state.playerMaxHealth}/>
+    <input type="number" id="playerMaxHealth" disabled={settingsDisabled} onChange={listeners.onUpdateSetting} defaultValue={state.playerMaxHealth} step="1" min="1" max="100"/>
     <label for="numDice">Number of Dice:</label>
-    <input type="number" id="numDice" disabled={settingsDisabled} onChange={listeners.onUpdateSetting} defaultValue={state.numDice}/>
+    <input type="number" id="numDice" disabled={settingsDisabled} onChange={listeners.onUpdateSetting} defaultValue={state.numDice} step="1" min="1" max="25"/>
     <label for="sharedResourceMax">Arrow Pile Total Size:</label>
-    <input type="number" id="sharedResourceMax" disabled={settingsDisabled} onChange={listeners.onUpdateSetting} defaultValue={state.sharedResourceMax}/>
+    <input type="number" id="sharedResourceMax" disabled={settingsDisabled} onChange={listeners.onUpdateSetting} defaultValue={state.sharedResourceMax} step="1" min="0" max="100"/>
     </div>
   )
 }
