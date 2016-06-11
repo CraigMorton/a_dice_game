7339a00db0ef6807fc3c20d75ec84bc8fac57b65
Craig Morton <CraigMorton92@gmail.com>
2016-06-11 14:36:33 +0100

    fixed typo in gameInfoReducer failing to update sharedResource.count

--- react_webapp/client/src/reducers/gameInfoReducer.js
+++ react_webapp/client/src/reducers/gameInfoReducer.js
@@ -9,8 +9,8 @@ const gameInfoReducer = (state = defaultState, action) => {
   const mapActionStringToActionDispatchCall = {
     "INCREMENT_DICE_ROLLED_COUNTER": () => ({ ...state, diceRolledCount: state.diceRolledCount + action.count }),
     "INCREMENT_TURNS_PLAYED_COUNTER": () => ({...state, turnsPlayedCount: state.turnsPlayedCount + action.count }),
-    "REMOVE_SHARED_RESOURCE_FROM_POOL": () => ({...state, count: state.sharedResource.count - 1 }),
-    "RESET_SHARED_RESOURCE_POOL": () => ({...state, count: state.sharedResource.max })
+    "REMOVE_SHARED_RESOURCE_FROM_POOL": () => ({...state, sharedResource: {count: state.sharedResource.count - 1} }),
+    "RESET_SHARED_RESOURCE_POOL": () => ({...state, sharedResource: {count: state.sharedResource.max} })
   }
   // if action type is on the reducer function map - this if will pass - and invoke the appropriate function, returning the return value of the appropriate function
   if (mapActionStringToActionDispatchCall[action.type]) return mapActionStringToActionDispatchCall[action.type]();
