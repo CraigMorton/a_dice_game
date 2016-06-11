8f6c1838663954c5c0bedab63d11a32cb5859197
Craig Morton <CraigMorton92@gmail.com>
2016-06-11 17:10:23 +0100

    PlayerListContainer orders players according to id instead of current order in state - player list stays still and active player moves, rather than the whole list moving round every turn

--- react_webapp/client/src/components/player_list/PlayerListContainer.jsx
+++ react_webapp/client/src/components/player_list/PlayerListContainer.jsx
@@ -6,9 +6,17 @@ const PlayerListContainer = ({
   listeners,
   currentPlayerTargetId
 }) => {
-  const playerListItems = state.map((player) => {
-    const isTargetted = (currentPlayerTargetId === player.player_id);
-    return (<PlayerListItem id={player.player_id} key={player.player_id} isTargetted={isTargetted} health={player.health} maxHealth={player.maxHealth} sharedResource={player.sharedResource} listener={listeners.onTargetSelect} onClick={listeners.onTargetSelect}/>)
+  let playerListItems = [];
+  console.log(playerListItems);
+  state.forEach((player, index) => {
+    const id = player.player_id
+    const isTargetted = (currentPlayerTargetId === id);
+    const isCurrentPlayer = (id === state[0].player_id)
+    playerListItems[id] = (<PlayerListItem id={id} key={id} isTargetted={isTargetted} isCurrentPlayer={isCurrentPlayer} health={player.health} maxHealth={player.maxHealth} sharedResource={player.sharedResource} listener={listeners.onTargetSelect} onClick={listeners.onTargetSelect}/>)
+  // const playerListItems = state.map((player) => {
+  //   const isTargetted = (currentPlayerTargetId === player.player_id);
+  //   const isCurrentPlayer = (player.player_id === state[0].player_id)
+  //   return (<PlayerListItem id={player.player_id} key={player.player_id} isTargetted={isTargetted} isCurrentPlayer={isCurrentPlayer} health={player.health} maxHealth={player.maxHealth} sharedResource={player.sharedResource} listener={listeners.onTargetSelect} onClick={listeners.onTargetSelect}/>)
   });
   return(
     <div className="react-container">
--- react_webapp/client/src/components/player_list/presentation/PlayerListItem.jsx
+++ react_webapp/client/src/components/player_list/presentation/PlayerListItem.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const PlayerListItem = ({
   listener,
   isTargetted,
+  isCurrentPlayer,
   id,
   health,
   maxHealth,
@@ -10,7 +11,7 @@ const PlayerListItem = ({
 }) => {
   return(
     <div onClick={listener} id={id}
-    style={{backgroundColor: isTargetted ? "tomato" : "skyblue"}}
+    style={{backgroundColor: isTargetted ? "tomato" : "skyblue", border: isCurrentPlayer ? "4px solid limegreen" : null}}
     >
     <p id={id}>Player ID: {id}</p>
     <p id={id}>Health: {health}/{maxHealth}</p>
