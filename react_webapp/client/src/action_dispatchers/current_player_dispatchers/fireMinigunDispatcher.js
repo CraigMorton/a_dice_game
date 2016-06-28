69dfe398e7effabc2a80fdf5086c214fd9fd3e13
Craig Morton <CraigMorton92@gmail.com>
2016-06-28 20:15:51 +0100

    firing minigun locks first three dice of appropriate value to prevent them being used again

--- react_webapp/client/src/action_dispatchers/current_player_dispatchers/fireMinigunDispatcher.js
+++ react_webapp/client/src/action_dispatchers/current_player_dispatchers/fireMinigunDispatcher.js
@@ -1,12 +1,21 @@
 import store from "../../store.js"
 import {fireMinigun} from "../../action_creators/currentPlayerActions.js";
 import {takeMinigunDamage} from "../../action_creators/playerListActions.js";
+import {lockDie} from "../../action_creators/currentPlayerActions.js";
 
 
 const fireMinigunDispatcher = (minigunAvailable) => {
   if (minigunAvailable === 1){
     store.dispatch(fireMinigun(minigunAvailable));
     store.dispatch(takeMinigunDamage(minigunAvailable));
+    const minigunDiceIndexes = [];
+    const dice = store.getState().currentPlayer.dice.slice();
+    for (let i = 0; i < dice.length; i++){
+      if (dice[i].value === 4) minigunDiceIndexes.push(i);
+    }
+    for (let i = 0; i < 3; i++){
+      store.dispatch(lockDie(minigunDiceIndexes[i]))
+    }
   }
 
 
