19848acfca7400aaf5f8c53e4f9302a584870a62
Craig Morton <CraigMorton22@gmail.com>
2016-12-19 11:42:41 +0000

    Add dead state to PlayerListContainer

--- react_webapp/client/src/components/player_list/PlayerListContainer.jsx
+++ react_webapp/client/src/components/player_list/PlayerListContainer.jsx
@@ -10,13 +10,32 @@ const PlayerListContainer = ({
   let playerListItems = [];
   state.forEach((player, index) => {
     const id = player.player_id
-    const distance = (index)
+    const distance = index
     const isInRange = {}
     isInRange.one = (index === 1 || index === state.length - 1);
     isInRange.two = (index === 2 || index === state.length - 2);
     const isTargetted = (currentPlayerTargetId === id);
     const isCurrentPlayer = (id === state[0].player_id)
+    if (!player.isDead) {
     playerListItems[id] = (<PlayerListItem id={id} key={id} stateIndex={index} isTargetted={isTargetted} isCurrentPlayer={isCurrentPlayer} health={player.health} maxHealth={player.maxHealth} sharedResource={player.sharedResource} onTarget={listeners.onTargetSelect} onActivateActionOnTarget={listeners.onActivateActionOnTarget} isDead={player.isDead} isInRange={isInRange} currentPlayerActionCounters={currentPlayerActionCounters}/>)
+    }
+    else if (player.isDead) {
+      playerListItems[id] = (
+        <PlayerListItem
+        id={id} key={id}
+        stateIndex={index}
+        isTargetted={isTargetted}
+        isCurrentPlayer={isCurrentPlayer}
+        health={"DEAD"}
+        maxHealth={player.maxHealth}
+        sharedResource={0}
+        // onTarget={listeners.onTargetSelect}
+        // onActivateActionOnTarget={listeners.onActivateActionOnTarget}
+        isDead={player.isDead}
+        isInRange={isInRange}
+        currentPlayerActionCounters={currentPlayerActionCounters}/>
+      )
+    }
     // LIST IN STATE ORDER BELOW - ABOVE IS IN PLAYER_ID ORDER
   // const playerListItems = state.map((player) => {
   //   const isTargetted = (currentPlayerTargetId === player.player_id);
